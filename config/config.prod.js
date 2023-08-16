3c907ef413d53376bfe59d462cd94c2470ed59ef
Carlos <2899952565@qq.com>
2023-08-17 02:47:50 +0800

    🐞 fix: redis port

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,3 +1,10 @@
+/*
+ * @Date: 2023-03-07 22:17:47
+ * @LastEditors: Carlos 2899952565@qq.com
+ * @LastEditTime: 2023-08-17 01:47:14
+ * @FilePath: /lx_ytb/config/config.prod.js
+ * @description:
+ */
 exports.vod = {
   accessKeyId: process.env.accessKeyId,
   accessKeySecret: process.env.accessKeySecret,
@@ -6,7 +13,7 @@ exports.vod = {
 
 exports.redis = {
   client: {
-    port: 6376, // Redis port
+    port: 6379, // Redis port
     host: '47.98.217.142', // Redis host
     password: 'lpx666888',
     db: 0,
